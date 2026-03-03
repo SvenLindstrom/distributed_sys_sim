5d089a70b595454958e27a5ebfcfddd3f4c0026f
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2026-03-03 16:06:36 +0100

    random id generator

---
+++ scheduler/src/misc/helper.go
@@ -0,0 +1,14 @@
+package misc
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func GenID() (string, error) {
+	b := make([]byte, 16)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+}
