051b3f6daf1d45ec70fe8f68108ee52ec39dd766
Bianca Casetta <biancacasetta@gmail.com>
2026-03-05 15:56:27 +0100

    added implementation folder with shared resources

---
+++ internal/network/rpc.go
@@ -0,0 +1,17 @@
+package network
+
+import "net/rpc"
+
+type RPCClient interface {
+	Call(serviceName string, args any, reply any) error
+}
+
+type RPCDialer interface {
+	Dial(address string) (RPCClient, error)
+}
+
+type RealRPCDialer struct{}
+
+func (d *RealRPCDialer) Dial(address string) (RPCClient, error) {
+	return rpc.DialHTTP("tcp", address)
+}
