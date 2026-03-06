0c9d39ab60bb76b32dc49ab1648e5192f739c0a6
Bianca Casetta <biancacasetta@gmail.com>
2026-03-06 15:13:43 +0100

    added main file for worker process

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"dssim/internal/misc"
+	"dssim/internal/worker"
+	"log"
+	"os"
+)
+
+func main() {
+
+	// initialise Logger
+	file, err := misc.Loginit("worker")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// get Worker's address
+	address := os.Getenv("HOSTNAME") + ":9000"
+
+	// create and run Worker
+	w := worker.NewWorker(address, "scheduler:9000")
+	w.Run()
+
+}
