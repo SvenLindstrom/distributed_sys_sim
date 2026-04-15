5ed3218de963fda05afffe0eed3674344ae2464a
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2026-04-15 15:30:27 +0200

    failover imple

--- internal/scheduler/scheduler_init.go
+++ internal/scheduler/scheduler_init.go
@@ -19,6 +19,8 @@ func InitScheduler(workerQueueSize int, taskQueueSize int) {
 		NewSchedular(workerQueueSize, taskQueueSize)
 	case "ELECTION-REPLICATION":
 		NewSchedulerRaft(workerQueueSize, taskQueueSize)
+	case "FAILOVER":
+		newSchedulerFailover(workerQueueSize, taskQueueSize)
 	}
 }
 
@@ -46,6 +48,26 @@ func leaderChange(leader <-chan bool, s *Scheduler, rpc *RpcInterface) {
 			s.RunLoop = false
 		}
 	}
+}
+
+func newSchedulerFailover(workerQueueSize int, taskQueueSize int) Scheduler {
+	println("In Failover")
+	workerManager := worker.NewWorkerManager(workerQueueSize)
+
+	fsm := state.NewSchedulerState(taskQueueSize)
+
+	r, err := NewRaft(fsm)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	s := NewScheduler(workerManager, fsm)
+
+	RpcInterface := NewRPC(workerManager, fsm)
+	go leaderChange(r.raft.LeaderCh(), &s, &RpcInterface)
+
+	return s
 }
 
 func NewSchedulerRaft(workerQueueSize int, taskQueueSize int) Scheduler {
